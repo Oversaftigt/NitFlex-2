ed681ad2379c31baf28305fac75856cd135764f2
Oversaftigt <32249246+Oversaftigt@users.noreply.github.com>
2024-11-19 01:42:14 +0100

    Identitymicroservice migration

---
+++ IdentityMicroservice/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityMicroservice.Data
+{
+    public class UserDbContext : IdentityDbContext
+    {
+
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
---
+++ IdentityMicroservice/Program.cs
@@ -0,0 +1,38 @@
+using IdentityMicroservice.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Dbcontext
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureSql")));
+
+//
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<UserDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
