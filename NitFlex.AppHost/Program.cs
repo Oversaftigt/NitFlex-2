f287a47982d5e034ebcedf56085dc0b747d030ba
Martin Ngo <mng@unik.dk>
2024-11-19 10:41:56 +0100

    Mere til apphost

--- NitFlex.AppHost/Program.cs
+++ NitFlex.AppHost/Program.cs
@@ -1,9 +1,21 @@
+//Tilføj Aspire apphost project, højreklik på microservicesprojeker og add aspire orchestration support eller hvad det hedder
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MovieMicroservice>("moviemicroservice");
+var stateStore = builder.AddDaprStateStore("statestore");
+var pubsub = builder.AddDaprPubSub("pubsub");
 
-builder.AddProject<Projects.RentalMicroservice>("rentalmicroservice");
+builder.AddProject<Projects.MovieMicroservice>("moviemicroservice")
+  .WithDaprSidecar()
+  .WithReference(pubsub);
 
-builder.AddProject<Projects.IdentityMicroservice>("identitymicroservice");
+
+builder.AddProject<Projects.RentalMicroservice>("rentalmicroservice")
+  .WithDaprSidecar()
+  .WithReference(pubsub);
+
+builder.AddProject<Projects.IdentityMicroservice>("identitymicroservice")
+  .WithDaprSidecar()
+  .WithReference(pubsub);
 
 builder.Build().Run();
