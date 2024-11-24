c19c77247ea70eaa4e59926282f596b5bc1c4831
Oversaftigt <32249246+Oversaftigt@users.noreply.github.com>
2024-11-24 16:18:30 +0100

    Empty blazor wasm tilføjet

--- IdentityMicroservice/Controllers/AccountController.cs
+++ IdentityMicroservice/Controllers/AccountController.cs
@@ -55,8 +55,8 @@ namespace IdentityMicroservice.Controllers
 
                 var authClaims = new List<Claim>
                 {
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(ClaimTypes.Email, user.Email),
                 };
 
                 authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
